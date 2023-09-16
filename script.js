ffc9707a510d23a1b7a7283c717e4aed75ea18fb
simonbrost <simonb@live.at>
2023-09-16 14:53:50 +0200

    second submit

---
+++ script.js
@@ -0,0 +1,47 @@
+let questions = [
+    { //6 Variablen (links vom Doppelpunkt) und 6 Werte (rechts davon)
+        // 5 dieser werte sind strings und die letzte ist ein integers (Buchstaben und zahlen)//
+        "question": "Wer hat HTML erfunden",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3,
+    },
+
+    {
+        "question": "Was bedeutet das HTML Tag ",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3,
+    },
+
+    {
+        "question": "Wie bindet man eine Website in eine Website ein",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3,
+    },
+
+    {
+        "question": "Wie stellt man Text am Besten fett dar",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3,
+    },
+
+    {
+        "question": "Wie definiert man in JavaScript eine Variable",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3,
+    }
+];
