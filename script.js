19735037fd6c2f3dbc0c3ccef7215550b2205a30
simonbrost <simonb@live.at>
2023-09-17 18:42:01 +0200

    more css

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ let questions = [
     }
 ];
 
+let rightQuestions = 0;
+
 let currentQuestion = 0;
 
 function init() {
@@ -60,6 +62,10 @@ function showQuestion() {
         //ToDo: show EndScreen
         document.getElementById('end-screen').style = '';
         document.getElementById('question-body').style = 'display: none';
+
+        document.getElementById('questions-all').innerHTML = questions.length;
+        document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+
     } else {
 
         let question = questions[currentQuestion];
@@ -81,6 +87,7 @@ function answer(selection) {
 
     if (selectedQuestionNumber == question['right_answer']) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        rightQuestions++;
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
